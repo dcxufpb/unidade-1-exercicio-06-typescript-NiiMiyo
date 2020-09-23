8521bec0c9fa2d9836bfe22be301ffd0ebdabca8
Nii Miyo <kagnanii.miyo@gmail.com>
2020-09-23 15:04:02 -0300

    Teste passando

--- cupom.test.ts
+++ cupom.test.ts
@@ -1,11 +1,11 @@
-const cupom = require('./cupom');
+const cupom = require("./cupom");
 
 function verificaCampoObrigatorio(mensagemEsperada: string) {
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(mensagemEsperada);
-  }
+	try {
+		cupom.dados_loja();
+	} catch (e) {
+		expect(e.message).toBe(mensagemEsperada);
+	}
 }
 
 // Todas as variáveis preenchidas
@@ -85,100 +85,107 @@ CNPJ: 11.111.111/1111-11
 IE: 123456789
 `;
 
-test('Loja Completa', () => {
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_LOJA_COMPLETA);
+test("Loja Completa", () => {
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_LOJA_COMPLETA);
 });
 
-test('Nome vazio', () => {
-  cupom.dados.nome_loja = "";
-  verificaCampoObrigatorio(`O campo nome da loja é obrigatório`);
-  cupom.dados.nome_loja = "Arcos Dourados Com. de Alimentos LTDA";
+test("Nome vazio", () => {
+	cupom.dados.nome_loja = "";
+	verificaCampoObrigatorio(`O campo nome da loja é obrigatório`);
+	cupom.dados.nome_loja = "Loja 1";
 });
 
-test('Logradouro vazio', () => {
-  cupom.dados.logradouro = "";
-  verificaCampoObrigatorio(`O campo logradouro do endereço é obrigatório`);
-  cupom.dados.logradouro = "Av. Projetada Leste";
+test("Logradouro vazio", () => {
+	cupom.dados.logradouro = "";
+	verificaCampoObrigatorio(`O campo logradouro do endereço é obrigatório`);
+	cupom.dados.logradouro = "Log 1";
 });
 
-test('Número zero', () => {
-  cupom.dados.numero = 0;
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_NUMERO);
-  cupom.dados.numero = 500;
+test("Número zero", () => {
+	cupom.dados.numero = 0;
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_NUMERO);
+	cupom.dados.numero = 10;
 });
 
-test('Complemento vazio', () => {
-  cupom.dados.complemento = "";
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_COMPLEMENTO);
-  cupom.dados.numero = "C1";
+test("Complemento vazio", () => {
+	cupom.dados.complemento = "";
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_COMPLEMENTO);
+	cupom.dados.complemento = "C1";
 });
 
-test('Bairro vazio', () => {
-  cupom.dados.bairro = "";
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_BAIRRO);
-  cupom.dados.bairro = "Bai 1";
+test("Bairro vazio", () => {
+	cupom.dados.bairro = "";
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_BAIRRO);
+	cupom.dados.bairro = "Bai 1";
 });
 
-test('Município vazio', () => {
-  cupom.dados.municipio = "";
-  verificaCampoObrigatorio(`O campo município do endereço é obrigatório`);
-  cupom.dados.municipio = "Campinas";
+test("Município vazio", () => {
+	cupom.dados.municipio = "";
+	verificaCampoObrigatorio(`O campo município do endereço é obrigatório`);
+	cupom.dados.municipio = "Mun 1";
 });
 
-test('Estado vazio', () => {
-  cupom.dados.estado = "";
-  verificaCampoObrigatorio(`O campo estado do endereço é obrigatório`);
-  cupom.dados.estado = "SP";
+test("Estado vazio", () => {
+	cupom.dados.estado = "";
+	verificaCampoObrigatorio(`O campo estado do endereço é obrigatório`);
+	cupom.dados.estado = "E1";
 });
 
-test('CEP vazio', () => {
-  cupom.dados.cep = "";
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_CEP);
-  cupom.dados.cep = "11111-111";
+test("CEP vazio", () => {
+	cupom.dados.cep = "";
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_CEP);
+	cupom.dados.cep = "11111-111";
 });
 
-test('Telefone vazio', () => {
-  cupom.dados.telefone = "";
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_TELEFONE);
-  cupom.dados.telefone = "(11) 1111-1111";
+test("Telefone vazio", () => {
+	cupom.dados.telefone = "";
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_TELEFONE);
+	cupom.dados.telefone = "(11) 1111-1111";
 });
 
-test('Observação vazia', () => {
-  cupom.dados.observacao = "";
-  expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_OBSERVACAO);
-  cupom.dados.observacao = "Obs 1";
+test("Observação vazia", () => {
+	cupom.dados.observacao = "";
+	expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_OBSERVACAO);
+	cupom.dados.observacao = "Obs 1";
 });
 
-test('CNPJ vazio', () => {
-  cupom.dados.cnpj = "";
-  verificaCampoObrigatorio(`O campo CNPJ da loja é obrigatório`);
-  cupom.dados.cnpj = "42.591.651/0797-34";
+test("CNPJ vazio", () => {
+	cupom.dados.cnpj = "";
+	verificaCampoObrigatorio(`O campo CNPJ da loja é obrigatório`);
+	cupom.dados.cnpj = "11.111.111/1111-11";
 });
 
-test('Inscrição estadual vazia', () => {
-  cupom.dados.inscricao_estadual = "";
-  verificaCampoObrigatorio(`O campo inscrição estadual da loja é obrigatório`);
-  cupom.dados.inscricao_estadual = "244.898.500.113";
+test("Inscrição estadual vazia", () => {
+	cupom.dados.inscricao_estadual = "";
+	verificaCampoObrigatorio(
+		`O campo inscrição estadual da loja é obrigatório`
+	);
+	cupom.dados.inscricao_estadual = "123456789";
 });
 
-test('Exercício 2 - customizado', () => {
+test("Exercício 2 - customizado", () => {
+	// Defina seus próprios valores para as variáveis a seguir
+	cupom.dados.nome_loja = "Top 10 nomes de lojas";
+	cupom.dados.logradouro = "Rua Tchurusbango Tchurusmago";
+	cupom.dados.numero = 13;
+	cupom.dados.complemento = "Do lado da casa vizinha";
+	cupom.dados.bairro = "Bairro do Limoeiro";
+	cupom.dados.municipio = "São Paulo";
+	cupom.dados.estado = "SP";
+	cupom.dados.cep = "08090-284";
+	cupom.dados.telefone = "(11) 4002-8922";
+	cupom.dados.observacao = "Entre o Campinho e a Lua de Baixo";
+	cupom.dados.cnpj = "43.745.249/0001-39";
+	cupom.dados.inscricao_estadual = "564.213.199.866";
 
-  // Defina seus próprios valores para as variáveis a seguir
-  cupom.dados.nome_loja = "";
-  cupom.dados.logradouro = "";
-  cupom.dados.numero = 0;
-  cupom.dados.complemento = "";
-  cupom.dados.bairro = "";
-  cupom.dados.municipio = "";
-  cupom.dados.estado = "";
-  cupom.dados.cep = "";
-  cupom.dados.telefone = "";
-  cupom.dados.observacao = "";
-  cupom.dados.cnpj = "";
-  cupom.dados.inscricao_estadual = "";
+	let expected = "Top 10 nomes de lojas\n";
+	expected += "Rua Tchurusbango Tchurusmago, 13 Do lado da casa vizinha\n";
+	expected += "Bairro do Limoeiro - São Paulo - SP\n";
+	expected += "CEP:08090-284 Tel (11) 4002-8922\n";
+	expected += "Entre o Campinho e a Lua de Baixo\n";
+	expected += "CNPJ: 43.745.249/0001-39\n";
+	expected += "IE: 564.213.199.866\n";
 
-  //E atualize o texto esperado abaixo
-  expect(cupom.dados_loja()).toBe(
-    `
-`);
+	//E atualize o texto esperado abaixo
+	expect(cupom.dados_loja()).toBe(expected);
 });
